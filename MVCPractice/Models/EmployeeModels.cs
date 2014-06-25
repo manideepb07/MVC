582f1bd6766d168b3e0a6ca25b46fa6cb0b2a66a
manideep <manideep.b07@hotmail.com>
2014-06-25 09:19:14 +0530

    MVC Practice update2 part 7 video

---
+++ MVCPractice/Models/EmployeeModels.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCPractice.Models
+{
+    public class EmployeeModels
+    {
+        public int EmployeeID { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string sex { get; set; }
+    }
+}
